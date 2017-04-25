edeae7f369f26f2f7dc5cc8ab96e4bcdfe33d07d
lisamsmith100 <lisamarie.smith@libertymutual.com>
2017-04-25 15:25:30 -0400

    Add code from code along

---
+++ scripts/doctors/update-appointment.sh
@@ -0,0 +1,9 @@
+curl --include --request PATCH http://localhost:4741/appointments/1 \
+  --header "Content-Type: application/json" \
+  --data '{
+    "appointment": {
+      "doctor_id": "2",
+      "patient_id": "5",
+      "date": "2017-05-01"
+    }
+  }'
